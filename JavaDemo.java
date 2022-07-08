47627c69066952b8704ce20e901bebe230892303
alexti <50894864+tianjunchang@users.noreply.github.com>
2022-07-09 01:31:47 +0800

    2022年7月9日 01:31:59 删除无用目录

--- JavaDemo.java
+++ JavaDemo.java
@@ -1,7 +1,5 @@
 public class JavaDemo {
     public static void main(String args[]) {
         System.out.println("测试项目能否正常运行");
-        System.out.println("测试项目能否正常运行");
-        System.out.println("测试项目能否正常运行");
     }
 }
