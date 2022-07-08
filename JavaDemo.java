088a32c67ed0b11698c246de38fc681ea7986162
alexti <50894864+tianjunchang@users.noreply.github.com>
2022-07-09 02:29:42 +0800

    2022年7月9日 02:29:46 新的提交

--- JavaDemo.java
+++ JavaDemo.java
@@ -1,6 +1,6 @@
 public class JavaDemo {
     public static void main(String args[]) {
         System.out.println("测试项目能否正常运行");
-        System.out.println("测试项目能否正常运行");
+        System.out.println("2022年7月9日 02:29:22 新的测试");
     }
 }
